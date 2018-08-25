a01566e4d7f000566ff0cc0d81a63817768b530c
ZXFrank <FrankZhong@github.com>
2018-08-25 17:51:23 +0800

    仿QQ运动计步器view

--- app/src/main/java/com/frank/customview/MainActivity.java
+++ app/src/main/java/com/frank/customview/MainActivity.java
@@ -1,7 +1,13 @@
 package com.frank.customview;
 
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.DecelerateInterpolator;
+
+import com.frank.customview.wight.stepview.StepView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final StepView stepview = findViewById(R.id.stepview);
+        stepview.setStepMax(5000);
+        //属性动画
+        ValueAnimator valueAnimator = ObjectAnimator.ofFloat(0, 3000);
+        valueAnimator.setInterpolator(new DecelerateInterpolator());//设置修饰动画效果 https://blog.csdn.net/jason0539/article/details/16370405
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float current = (float) animation.getAnimatedValue();
+                stepview.setStepCurrent((int) current);
+            }
+        });
+        valueAnimator.setDuration(1000);
+        valueAnimator.start();
     }
 }
--- app/src/main/java/com/frank/customview/wight/stepview/StepView.java
+++ app/src/main/java/com/frank/customview/wight/stepview/StepView.java
@@ -6,6 +6,7 @@ import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
@@ -25,7 +26,10 @@ public class StepView extends View {
     private int mBorderWidth = 20;//圆弧宽度    px
     private int mStepTextSize;//步数字体大小
     private int mStepTextColor = Color.RED;//步数字体颜色
-    private Paint mEntiretyPaint;
+    private Paint mEntiretyPaint, mPortionPanit, mTextPanit;
+    private int mStepMax = 100;
+    private int mStepCurrent = 50;
+
 
     public StepView(Context context) {
         this(context, null);
@@ -54,7 +58,20 @@ public class StepView extends View {
         mEntiretyPaint.setAntiAlias(true);//设置抗锯齿
         mEntiretyPaint.setColor(mEntiretyColor);
         mEntiretyPaint.setStrokeWidth(mBorderWidth);
-        mEntiretyPaint.setStyle(Paint.Style.FILL);//画笔实心
+        mEntiretyPaint.setStrokeCap(Paint.Cap.ROUND);//边缘圆弧
+        mEntiretyPaint.setStyle(Paint.Style.STROKE);//画笔空心
+
+        mPortionPanit = new Paint();
+        mPortionPanit.setAntiAlias(true);//设置抗锯齿
+        mPortionPanit.setColor(mPortionColor);
+        mPortionPanit.setStrokeWidth(mBorderWidth);
+        mPortionPanit.setStrokeCap(Paint.Cap.ROUND);//边缘圆弧
+        mPortionPanit.setStyle(Paint.Style.STROKE);//画笔空心
+
+        mTextPanit = new Paint();
+        mPortionPanit.setAntiAlias(true);//设置抗锯齿
+        mTextPanit.setColor(mStepTextColor);
+        mTextPanit.setTextSize(mStepTextSize);
 
         //7.其他。。。
 
@@ -79,7 +96,40 @@ public class StepView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         //画外圆弧
-        RectF rectF = new RectF(getWidth(),0,getHeight(),0);
-        canvas.drawArc(rectF,135,270,false,mEntiretyPaint);
+        int center = getWidth() / 2;//中心点
+        int radius = getWidth() / 2 - mBorderWidth / 2;//半径
+//        RectF rectF = new RectF(mBorderWidth / 2, mBorderWidth / 2,
+//                getWidth() - mBorderWidth / 2, getHeight() - mBorderWidth / 2);
+        RectF rectF = new RectF(center - radius, center - radius, center + radius, center + radius);
+        canvas.drawArc(rectF, 135, 270, false, mEntiretyPaint);
+        //画内圆弧 不能写死 百分比
+        if (mStepMax == 0) return;
+        float sweepAngle = (float) mStepCurrent / mStepMax;
+        canvas.drawArc(rectF, 135, sweepAngle * 270, false, mPortionPanit);
+        //画文字
+        String stepText = mStepCurrent + "";
+        Rect rect = new Rect();
+        mTextPanit.getTextBounds(stepText, 0, stepText.length(), rect);
+        int dx = getWidth() / 2 - rect.width() / 2;
+        //基线 baseLine
+        Paint.FontMetricsInt fontMetricsInt = mTextPanit.getFontMetricsInt();
+        int dy = (fontMetricsInt.bottom - fontMetricsInt.top) / 2 - fontMetricsInt.bottom;
+        int baseLine = getHeight() / 2 + dy;
+        canvas.drawText(stepText, dx, baseLine, mTextPanit);
     }
+
+    /**
+     * 设置范围值
+     * @param stepMax
+     */
+    public void setStepMax(int stepMax){
+        this.mStepMax = stepMax;
+    }
+
+    public void setStepCurrent(int stepCurrent){
+        this.mStepCurrent = stepCurrent;
+        invalidate();//不断绘制
+    }
+
+
 }
